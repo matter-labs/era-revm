d42d073ec86f85a7b3df210f9247f2c0d32cde66
jrigada <jrigada@est.frba.utn.edu.ar>
2023-12-06 13:18:24 +0000

    add warp and set nonce cheatcode tests

--- tests/src/TestCheatcodes.sol
+++ tests/src/TestCheatcodes.sol
@@ -57,3 +57,45 @@ contract CheatcodeRollTest is Test {
         console.log("failed?", failed());
     }
 }
+
+contract CheatcodeWarpTest is Test {
+    uint256 constant NEW_BLOCK_TIMESTAMP = uint256(10000);
+
+    function test_warp() public {
+        uint256 initialTimestamp = block.timestamp;
+        console.log("timestamp before:", initialTimestamp);
+        require(
+            NEW_BLOCK_TIMESTAMP != initialTimestamp,
+            "timestamp must be different than current block timestamp"
+        );
+
+        (bool success, ) = CHEATCODE_ADDRESS.call(
+            abi.encodeWithSignature("warp(uint256)", NEW_BLOCK_TIMESTAMP)
+        );
+        require(success, "warp failed");
+
+        uint256 finalTimestamp = block.timestamp;
+        console.log("timestamp after:", finalTimestamp);
+        require(
+            finalTimestamp == NEW_BLOCK_TIMESTAMP,
+            "timestamp was not changed"
+        );
+    }
+}
+
+contract CheatcodeSetNonceTest is Test {
+    address constant TEST_ADDRESS = 0x6Eb28604685b1F182dAB800A1Bfa4BaFdBA8a79a;
+    uint256 constant NEW_NONCE = uint256(10000);
+
+    function test_setNonce() public {
+        (bool success, ) = CHEATCODE_ADDRESS.call(
+            abi.encodeWithSignature(
+                "setNonce(address,uint64)",
+                TEST_ADDRESS,
+                NEW_NONCE
+            )
+        );
+        require(success, "setNonce failed");
+        /// we should assert over the set nonce once we have a way to read it using getNonce
+    }
+}

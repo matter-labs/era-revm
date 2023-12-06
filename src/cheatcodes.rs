5a955d8a50e1d3a70c08d16d551c1cf2f9ceeddf
aon <21188659+aon@users.noreply.github.com>
2023-12-06 11:06:03 -0300

    feat: add working storage helper and vm.etch cheatcode

--- src/cheatcodes.rs
+++ src/cheatcodes.rs
@@ -9,14 +9,15 @@ use multivm::vm_refunds_enhancement::{
 use multivm::zk_evm_1_3_3::tracing::{BeforeExecutionData, VmLocalStateData};
 use std::fmt::Debug;
 use zk_evm::zkevm_opcode_defs::{FatPointer, Opcode, CALL_IMPLICIT_CALLDATA_FAT_PTR_REGISTER};
-use zksync_basic_types::{AccountTreeId, H160};
-use zksync_state::{ReadStorage, StoragePtr, StorageView, WriteStorage};
+use zksync_basic_types::{AccountTreeId, H160, H256, U256};
+use zksync_state::{ReadStorage, StoragePtr, StorageView};
 use zksync_types::{
     block::{pack_block_info, unpack_block_info},
     get_code_key, get_nonce_key,
     utils::{decompose_full_nonce, nonces_to_full_nonce, storage_key_for_eth_balance},
     StorageKey,
 };
+use zksync_types::{LogQuery, Timestamp};
 use zksync_utils::{h256_to_u256, u256_to_h256};
 
 // address(uint160(uint256(keccak256('hevm cheat code'))))
@@ -38,7 +39,17 @@ abigen!(
 );
 
 #[derive(Debug, Clone)]
-enum FinishCycleActions {}
+enum FinishCycleActions {
+    StorageWrite {
+        key: StorageKey,
+        read_value: H256,
+        write_value: H256,
+    },
+    StoreFactoryDep {
+        hash: U256,
+        bytecode: Vec<U256>,
+    },
+}
 
 #[derive(Clone, Debug, Default)]
 pub struct CheatcodeTracer {
@@ -95,10 +106,38 @@ impl<S: std::fmt::Debug + ForkSource, H: HistoryMode> VmTracer<ForkStorageView<S
 {
     fn finish_cycle(
         &mut self,
-        _state: &mut ZkSyncVmState<ForkStorageView<S>, H>,
+        state: &mut ZkSyncVmState<ForkStorageView<S>, H>,
         _bootloader_state: &mut BootloaderState,
     ) -> TracerExecutionStatus {
-        while let Some(_action) = self.actions.pop() {}
+        while let Some(action) = self.actions.pop() {
+            match action {
+                FinishCycleActions::StorageWrite {
+                    key,
+                    read_value,
+                    write_value,
+                } => {
+                    state.storage.write_value(LogQuery {
+                        timestamp: Timestamp(state.local_state.timestamp),
+                        tx_number_in_block: state.local_state.tx_number_in_block,
+                        aux_byte: Default::default(),
+                        shard_id: Default::default(),
+                        address: *key.address(),
+                        key: h256_to_u256(*key.key()),
+                        read_value: h256_to_u256(read_value),
+                        written_value: h256_to_u256(write_value),
+                        rw_flag: true,
+                        rollback: false,
+                        is_service: false,
+                    });
+                }
+                FinishCycleActions::StoreFactoryDep { hash, bytecode } => {
+                    state.decommittment_processor.populate(
+                        vec![(hash, bytecode)],
+                        Timestamp(state.local_state.timestamp),
+                    )
+                }
+            }
+        }
         TracerExecutionStatus::Continue
     }
 }
@@ -119,32 +158,22 @@ impl CheatcodeTracer {
         use CheatcodeContractCalls::*;
         match call {
             Deal(DealCall { who, new_balance }) => {
-                tracing::info!("Setting balance for {who:?} to {new_balance}");
-                storage
-                    .borrow_mut()
-                    .set_value(storage_key_for_eth_balance(&who), u256_to_h256(new_balance));
+                tracing::info!("👷 Setting balance for {who:?} to {new_balance}");
+                self.write_storage(
+                    storage_key_for_eth_balance(&who),
+                    u256_to_h256(new_balance),
+                    &mut storage.borrow_mut(),
+                );
             }
             Etch(EtchCall { who, code }) => {
-                tracing::info!("Setting address code for {who:?}");
+                tracing::info!("👷 Setting address code for {who:?}");
                 let code_key = get_code_key(&who);
-                let (hash, _code) = bytecode_to_factory_dep(code.0.into());
-                let hash = u256_to_h256(hash);
-
-                // TODO: store factory dep
-                // self.revm_db.store_factory_dep(
-                //     hash,
-                //     code.iter()
-                //         .flat_map(|entry| {
-                //             let mut bytes = vec![0u8; 32];
-                //             entry.to_big_endian(&mut bytes);
-                //             bytes.to_vec()
-                //         })
-                //         .collect(),
-                // );
-                storage.borrow_mut().set_value(code_key, hash);
+                let (hash, code) = bytecode_to_factory_dep(code.0.into());
+                self.store_factory_dep(hash, code);
+                self.write_storage(code_key, u256_to_h256(hash), &mut storage.borrow_mut());
             }
             SetNonce(SetNonceCall { account, nonce }) => {
-                tracing::info!("Setting nonce for {account:?} to {nonce}");
+                tracing::info!("👷 Setting nonce for {account:?} to {nonce}");
                 let mut storage = storage.borrow_mut();
                 let nonce_key = get_nonce_key(&account);
                 let full_nonce = storage.read_value(&nonce_key);
@@ -174,10 +203,10 @@ impl CheatcodeTracer {
                     account,
                     nonce
                 );
-                storage.set_value(nonce_key, u256_to_h256(enforced_full_nonce));
+                self.write_storage(nonce_key, u256_to_h256(enforced_full_nonce), &mut storage);
             }
             Roll(RollCall { block_number }) => {
-                tracing::info!("Setting block number to {}", block_number);
+                tracing::info!("👷 Setting block number to {}", block_number);
 
                 let key = StorageKey::new(
                     AccountTreeId::new(zksync_types::SYSTEM_CONTEXT_ADDRESS),
@@ -186,13 +215,14 @@ impl CheatcodeTracer {
                 let mut storage = storage.borrow_mut();
                 let (_, block_timestamp) =
                     unpack_block_info(h256_to_u256(storage.read_value(&key)));
-                storage.set_value(
+                self.write_storage(
                     key,
                     u256_to_h256(pack_block_info(block_number.as_u64(), block_timestamp)),
+                    &mut storage,
                 );
             }
             Warp(WarpCall { timestamp }) => {
-                tracing::info!("Setting block timestamp {}", timestamp);
+                tracing::info!("👷 Setting block timestamp {}", timestamp);
 
                 let key = StorageKey::new(
                     AccountTreeId::new(zksync_types::SYSTEM_CONTEXT_ADDRESS),
@@ -200,11 +230,30 @@ impl CheatcodeTracer {
                 );
                 let mut storage = storage.borrow_mut();
                 let (block_number, _) = unpack_block_info(h256_to_u256(storage.read_value(&key)));
-                storage.set_value(
+                self.write_storage(
                     key,
                     u256_to_h256(pack_block_info(block_number, timestamp.as_u64())),
+                    &mut storage,
                 );
             }
         };
+    }
+
+    fn store_factory_dep(&mut self, hash: U256, bytecode: Vec<U256>) {
+        self.actions
+            .push(FinishCycleActions::StoreFactoryDep { hash, bytecode });
+    }
+
+    fn write_storage<S: std::fmt::Debug + ForkSource>(
+        &mut self,
+        key: StorageKey,
+        write_value: H256,
+        storage: &mut StorageView<ForkStorage<S>>,
+    ) {
+        self.actions.push(FinishCycleActions::StorageWrite {
+            key,
+            read_value: storage.read_value(&key),
+            write_value,
+        });
     }
 }
